5efd0cd37ce23730843079c10e31b30c3fccd10a
FANO <121119011+FANO-OFFICIAL@users.noreply.github.com>
2025-08-29 08:09:49 +0000

    Utilisez les Outlets pour afficher certaines parties de la page

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import Home from "./pages/Home/";
 import Survey from "./pages/Survey/";
 import Header from "./components/Header";
 
+// On ajoute nos composants
+
+import ClientForm from "./components/ClientForm";
+import FreelanceForm from "./components/FreelanceForm";
+
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
@@ -18,7 +23,11 @@ root.render(
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/survey" element={<Survey />} />
+        <Route path="/survey" element={<Survey />}>
+          {/* Nous imbriquons nos composants dans survey */}
+          <Route path="client" element={<ClientForm />} />
+          <Route path="freelance" element={<FreelanceForm />} />
+        </Route>
       </Routes>
     </Router>
   </StrictMode>
