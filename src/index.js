8c28e3ff433abb169817f023751c422be696f3af
FANO <121119011+FANO-OFFICIAL@users.noreply.github.com>
2025-08-29 08:36:54 +0000

    Indiquez les types de vos props avec les PropTypes

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import Home from "./pages/Home/";
 import Survey from "./pages/Survey/";
 import Header from "./components/Header";
 import Error from "./components/Error";
+import Results from "./pages/Results";
+import Freelances from "./pages/Freelances";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -18,6 +20,8 @@ root.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/freelances" element={<Freelances />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
