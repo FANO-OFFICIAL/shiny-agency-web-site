16d5a912c20b66de7e78903eddaa3d6fdeb376d7
FANO <121119011+FANO-OFFICIAL@users.noreply.github.com>
2025-08-29 08:25:37 +0000

    Créez une route pour les attraper toutes : 404

--- src/index.js
+++ src/index.js
@@ -6,28 +6,19 @@ import App from "./App";
 import Home from "./pages/Home/";
 import Survey from "./pages/Survey/";
 import Header from "./components/Header";
-
-// On ajoute nos composants
-
-import ClientForm from "./components/ClientForm";
-import FreelanceForm from "./components/FreelanceForm";
+import Error from "./components/Error";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    {/*<App />*/}
-    {/*<Home />*/}
     <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/survey" element={<Survey />}>
-          {/* Nous imbriquons nos composants dans survey */}
-          <Route path="client" element={<ClientForm />} />
-          <Route path="freelance" element={<FreelanceForm />} />
-        </Route>
+        <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   </StrictMode>
